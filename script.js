b85f7a7e8787df5bab8db5e73e3f90bcb0482785
Adewu233 <adewuyioyinlola@gmail.com>
2022-11-11 21:44:05 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,44 @@
+$('.owl-carousel').owlCarousel({
+    autoplay: true,
+    autoplayTimeout: 5000,
+    autoplayHoverPause: true,
+    loop: true,
+    margin: 50,
+    responsiveClass: true,
+    nav: true,
+    loop: true,
+    stagePadding: 100,
+    responsive: {
+      0: {
+        items: 1
+      },
+      568: {
+        items: 2
+      },
+      600: {
+        items: 3
+      },
+      1000: {
+        items: 4
+    },
+    1000: {
+      items: 5
+    },
+    1000: {
+      items: 6
+      }
+    }
+  })
+  $(document).ready(function() {
+    $('.popup-youtube').magnificPopup({
+      disableOn: 320,
+      type: 'iframe',
+      mainClass: 'mfp-fade',
+      removalDelay: 160,
+      preloader: false,
+      fixedContentPos: true
+    });
+  });
+  $('.item').magnificPopup({
+    delegate: 'a',
+  });
